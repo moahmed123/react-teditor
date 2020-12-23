a001028fcd057960475ad4c12a35e3671da12c5a
Mohamed Alaa <mohamedalaaabas93@gmail.com>
2020-12-23 18:02:18 +0200

    fixed when drop show

--- src/actions/Api_paths.js
+++ src/actions/Api_paths.js
@@ -1,41 +1,4 @@
-// // At Live App Version Use:
-// const ApiPaths = {
-//     //API Links
-//     getTemplateStructureV2:'/admin/teditor/teditorV2/getTemplateStructure',
-//     getSectionFieldsV2:"/admin/teditor/teditorV2/getSectionFields",
-//     getLookupKey: "/admin/teditor/teditorV2/getLookup",
-//     searchCategories: "/admin/teditor/teditorV2/searchCategories",
-//     searchProducts: "/admin/teditor/teditorV2/searchProducts",
-//     searchLink: "/admin/teditor/teditorV2/getLinks",
-//     SaveFieldsVal: "/admin/teditor/teditorV2/SaveFieldsVal",
-//     addCollection: "/admin/teditor/teditorV2/AddCollection",
-//     removeCollection:"/admin/teditor/teditorV2/RemoveCollection",
-//     reorderCollections: '/admin/teditor/teditorV2/ReorderCollections',
-//     getLanguages: "/admin/teditor/teditorV2/GetLanguages",    
-//     publish: "/admin/teditor/teditorV2/Publish",
-//     removeSection: '/admin/teditor/teditorV2/RemoveSection',
-//     reorderSection: "/admin/teditor/teditorV2/ReorderSections",
-//     getTemplateSettings: "/admin/teditor/teditorV2/getTemplateSettings",
-//     getPageAvailableSections: '/admin/teditor/teditorV2/getPageAvailableSections',
-//     insertSection: "/admin/teditor/teditorV2/insertSection",
-//     addLayout: "/admin/teditor/teditorV2/AddLayout",
-//     UpdateSectionState: "/admin/teditor/teditorV2/UpdateSectionState",
-//     resetDraftVersion: "/admin/teditor/teditorV2/ResetDraftVersion",
-//     searchBrands: "/admin/teditor/teditorV2/searchBrands",
-//     setLanguage: "/admin/common/installation/language",
-//     // Paths 
-//     IframeUrl: "/index.php?",
-//     Paths: '/admin/teditor/teditorV2/', // ex '/admin/teditor/teditor/'
-//     AdminPaths: '/admin/', // back To Admin ex '/admin'
-    
-//     ecdata : "/ecdata/stores/",
-//     srcClearImage : '/image/cache/no_image-235x235.jpg',
-//     saveImageData : '/image/', 
 
-//     URlMediaManager: '/',
-//     IframeLink: ''
-// };
-// export default ApiPaths;
 
 // At Demo App Version Use:
 const ApiPaths = {
