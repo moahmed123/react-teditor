295131f21d0f2ddee17dc4cc60d5ca57f2091dc0
minaexpandcart <mina.malak@expandcart.com>
2021-02-10 15:27:48 +0200

    render scripts returned for edited sections

--- src/actions/Iframe/replaceIframeSection.js
+++ src/actions/Iframe/replaceIframeSection.js
@@ -1,10 +1,17 @@
 import { refreshIframe } from './refreshIframe';
 import $ from "jquery-1.10.2";
 
-export const replaceIframeSection = (sectionId, sectionHTML) => (dispatch) => {
+export const replaceIframeSection = (sectionId, sectionHTML, sectionScripts) => (dispatch) => {
     // Refresh Iframe When Save.
     if(sectionHTML){
         $('.iframe-site').contents().find('#section-' + sectionId).replaceWith(sectionHTML);
+        if(sectionScripts){
+			var myIframe = document.getElementsByClassName("iframe-site")[0];
+			var script = myIframe.contentWindow.document.createElement("script");
+			script.type = "text/javascript";
+			script.innerHTML = sectionScripts;
+			myIframe.contentWindow.document.body.appendChild(script);
+        }
     }  
     else{
         dispatch(refreshIframe()) 
